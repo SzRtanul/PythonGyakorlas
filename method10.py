d5ab6800c2001e3ec934b7385dd70faeb6d51dfd
Szabó Roland <rolandszb111@gmail.com>
2023-10-28 22:14:00 +0200

    Kész!

--- method10.py
+++ method10.py
@@ -1,8 +1,28 @@
 from inputs import *
 
+def f10(arr: []):
+    atl = 0
+    i = 0
+    while (i < len(arr)):
+        atl += arr[i]
+        i += 1
+    return atl / i
+
 def index():
     print(inputTexts(9, 0))
-    
+    arr = []
+    x = 1
+    i = 0
+    while(x != 0):
+        try:
+            x = float(input(f"Kérem a(z) {i+1}. számot, ami 0-tól eltér: "))
+            if x != 0:
+                arr.append(x)
+            i += 0
+        except:
+            print("HIBA: Ez nem szám!")
+    print(f"A számok átlaga: {f10(arr)}")
+
 
 if __name__ == '__main__':
     index()
--- method11.py
+++ method11.py
@@ -1,7 +1,24 @@
 from inputs import *
+import method10
 
 def index():
     print(inputTexts(10, 0))
+    arr = []
+    x = 1
+    i = 0
+    while (x != 0):
+        try:
+            x = float(input(f"Kérem a(z) {i + 1}. számot, ami 0-tól eltér: "))
+            if x > 0:
+                arr.append(x)
+            elif x == 0:
+                pass
+            else:
+                print("Csak pozitív szám elfogadott: ")
+            i += 0
+        except:
+            print("HIBA: Ez nem szám!")
+    print(f"A számok átlaga: {method10.f10(arr)}")
 
 
 if __name__ == '__main__':
